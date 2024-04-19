ccd63e342cdbe027f4eefed0a0879c80464ae8d0
Andy Freshnock <afreshnock@clymene.beocat.ksu.edu>
2024-04-19 14:55:48 -0500

    New branch with some pthread experiments

--- 3way-pthread/pt1.c
+++ 3way-pthread/pt1.c
@@ -2,170 +2,162 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <time.h>
 
 #define NUM_THREADS 4
 
-#define ARRAY_SIZE 2000000
-#define STRING_SIZE 16
-#define ALPHABET_SIZE 26
+pthread_mutex_t mutex;		// mutex for theadTime
 
-pthread_mutex_t mutexsum;			// mutex for char_counts
+double threadTime[NUM_THREADS] = {};
 
-char char_array[ARRAY_SIZE][STRING_SIZE];
-int char_counts[ALPHABET_SIZE];			// count of individual characters
+int threadInfo[NUM_THREADS][3] = {};
 
-char getRandomChar()
+int maxCharArray[1000000] = {};
+
+char *src_file = "/homes/dan/625/wiki_dump.txt";
+//char *src_file = "names.txt";
+
+void *getLineAndMax(void *myID)
 {
-	int randNum = 0;
-	char randChar = ' ';
+    int threadNumber = (int*)myID;
+    printf("Thread: %d", threadNumber);
 
-	randNum = ALPHABET_SIZE * (rand() / (RAND_MAX + 1.0)); 	// pick number 0 < # < 25
-	randNum = randNum + 97;				// scale to 'a'
-	randChar = (char) randNum;
-
-	// printf("%c", randChar);
-	return randChar;
-}
-
-void init_arrays()
-{
-  int i, j; 
-
-  pthread_mutex_init(&mutexsum, NULL);
-
-  for ( i = 0; i < ARRAY_SIZE; i++) {
-	for ( j = 0; j < STRING_SIZE; j++ ) {
-		 char_array[i][j] = getRandomChar();
-	}
-  }
-
-  for ( i = 0; i < ALPHABET_SIZE; i++ ) {
-  	char_counts[i] = 0;
-  }
-}
-
-void *count_array(void *myID)
-{
-  char theChar;
-  int i, j, charLoc;
-  int local_char_count[ALPHABET_SIZE];
-
-  int startPos = ((int) myID) * (ARRAY_SIZE / NUM_THREADS);
-  int endPos = startPos + (ARRAY_SIZE / NUM_THREADS);
-
-  printf("myID = %d startPos = %d endPos = %d \n", (int) myID, startPos, endPos);
-
-					// init local count array
-  for ( i = 0; i < ALPHABET_SIZE; i++ ) {
-  	local_char_count[i] = 0;
-  }
-					// count up our section of the global array
-  for ( i = startPos; i < endPos; i++) {
-	for ( j = 0; j < STRING_SIZE; j++ ) {
-	         theChar = char_array[i][j];
-		 charLoc = ((int) theChar) - 97;
-		 local_char_count[charLoc]++;
-	}
-  }
-					// sum up the partial counts into the global arrays
-  pthread_mutex_lock (&mutexsum);
-  for ( i = 0; i < ALPHABET_SIZE; i++ ) {
-     char_counts[i] += local_char_count[i];
-  }
-  pthread_mutex_unlock (&mutexsum);
-
-  pthread_exit(NULL);
-}
-
-void print_results()
-{
-  int i,j, total = 0;
-
-  					// then print out the totals
-  for ( i = 0; i < ALPHABET_SIZE; i++ ) {
-     total += char_counts[i];
-     printf(" %c %d\n", (char) (i + 97), char_counts[i]);
-  }
-  printf("\nTotal characters:  %d\n", total);
-}
-
-//This function takes a source file and desination array and copys the file info into the array
-bool array_deserialize(const char *src_file, void *dst_data, const size_t elem_size, const size_t elem_count)
-{
-    if(src_file != NULL && dst_data != NULL && elem_size != 0 && elem_count != 0){//Checks arguments
-        if(strchr(src_file, '\n') != NULL){//sscanf(src_file, "\n") == 0){I tried to use this but it does not work
-	        return false;
-	    }
-    	FILE *file = fopen(src_file, "r");//trys to open file on read only
-            if(file != NULL){//Checks to see if file is opened
-				printf("opened");
-                int num = fread(dst_data, elem_size, elem_count, file);//reads the file into data array
-                fclose(file);//closes file
-    	    if(num >= elem_count){//checks to see if the correct amount of elements have been read over
-    	    	return true;//returns true if the correct amount of elements have been read over
-    	    }
-    	}
+    FILE *file = fopen(src_file, "r");
+    if (file == NULL)
+    {
+        printf("File did not open");
+        exit(-1);
     }
-    return false;//returns false if any problems 
+    int startLine = threadInfo[threadNumber][0];
+    int endLine = threadInfo[threadNumber][1];
+
+    fseek(file, startLine, SEEK_SET); // need rework
+
+    int lineNum = startLine;
+    char *line[1000] = {};
+    size_t len = 0;
+    //ssize_t read;
+
+    clock_t start = clock();
+
+    
+
+    while ( lineNum < endLine)
+    {
+        
+        pthread_mutex_lock(&mutex);
+        for(int i = 0; i < 1000; i++){ 
+            if(getline(&line[i], &len, file) == -1){
+                printf("End of File");
+                break;
+            }
+        } 
+        pthread_mutex_unlock(&mutex);
+        for(int j =0; j <1000;j++){
+
+            
+            int i = 0;
+            char currentCharacter = line[j][i];
+            char maxCharacter = line[j][i];
+            while (currentCharacter != '\n')
+            {
+                if ((int)currentCharacter > (int)maxCharacter)
+                {
+                    maxCharacter = currentCharacter;
+                }
+                i++;
+                currentCharacter = line[j][i];
+            }    
+            maxCharArray[lineNum] = maxCharacter;
+            //free(line[j]);
+        }
+        lineNum+= 1000;
+    }
+    
+    fclose(file);
+
+    clock_t end = clock();
+    double elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+   // pthread_mutex_lock(&mutex);
+    threadTime[threadNumber] = elapsedTime;
+    //pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
 }
 
-//This function given a line of characters returns the max ascii value from all the characters in the line
-void getLineAndMax(char* line, int* max){
-	int i = 0;
-	char currentCharacter = line[i];
-	char maxCharacter = line[i];
-	while(currentCharacter != '\n'){
-		if((int)currentCharacter > (int)maxCharacter){
-			maxCharacter = currentCharacter;
-		}
-		i++;
-		currentCharacter = line[i];
-	}
-	max = (int)maxCharacter;
-}
-
-main() {
+int main() {
 	int i, rc;
 	pthread_t threads[NUM_THREADS];
 	pthread_attr_t attr;
 	void *status;
-
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
 	/* Initialize and set thread detached attribute */
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	
-	char* src_file = "~/../dan/625/wiki_dump.txt";
 
-	bool deserialized = array_deserialize(src_file, char_array, STRING_SIZE, ARRAY_SIZE);
-	//init_arrays();
-	if(deserialized){
-		for (i = 0; i < NUM_THREADS; i++ ) {
-			//rc = pthread_create(&threads[i], &attr, count_array, (void *)i);  
-			rc = pthread_create(&threads[i], &attr, getLineAndMax, (void *)i);  
-			if (rc) {
-				printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-			}
-		}
+	pthread_mutex_init(&mutex, NULL);
 
-		/* Free attribute and wait for the other threads */
-		pthread_attr_destroy(&attr);
-		for(i=0; i<NUM_THREADS; i++) {
-			rc = pthread_join(threads[i], &status);
-			if (rc) {
-			printf("ERROR; return code from pthread_join() is %d\n", rc);
-			exit(-1);
-			}
-		}
+    FILE *file = fopen(src_file, "r");
+    if (file == NULL)
+    {
+        printf("File did not open");
+        exit(-1);
+    }
 
-		print_results();
+    int totalLines = 0;
+    fseek(file, 0, SEEK_SET);
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        totalLines++;
+    }
+    fclose(file);
 
-		pthread_mutex_destroy(&mutexsum);
-		printf("Main: program completed. Exiting.\n");
-		pthread_exit(NULL);
+    int linesPerThread = totalLines / NUM_THREADS;
+
+	for (i = 0; i < NUM_THREADS; i++ ) {
+        int endLine, startLine;
+
+        startLine = i * linesPerThread; 
+        endLine = (i+1) * linesPerThread;
+
+        threadInfo[i][0] = startLine;
+        threadInfo[i][1] = endLine;
+        threadInfo[i][2] = i;
+
+	    rc = pthread_create(&threads[i], &attr, getLineAndMax, (void *)i);
+	    if (rc) {
+	        printf("ERROR; return code from pthread_create() is %d\n", rc);
+		    exit(-1);
+	    }
 	}
-	printf("failed deserialize");
+
+	/* Free attribute and wait for the other threads */
+	pthread_attr_destroy(&attr);
+	for(i=0; i<NUM_THREADS; i++) {
+	     rc = pthread_join(threads[i], &status);
+	     if (rc) {
+		   printf("ERROR; return code from pthread_join() is %d\n", rc);
+		   exit(-1);
+	     }
+	}
+
+	printf("Average Lines Per Thread: %d\n", linesPerThread);
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        printf("Thread: %d, Time: %lf\n", i, threadTime[i]);
+    }
+
+    /* This is for the assignment
+    for (i = 0; i < sizeof(maxCharArray);i++){
+        printf("%d", maxCharArray[i]);
+    }
+    */
+	pthread_mutex_destroy(&mutex);
+	printf("Main: program completed. Exiting.\n");
+	pthread_exit(NULL);
 }
 
--- pt1.c
+++ pt1.c
@@ -2,61 +2,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <time.h>
 
-#define NUM_THREADS 8						// Number of threads. Can be changed for scale ability
+#define NUM_THREADS 8
 
-pthread_mutex_t mutex;						// mutex for reading the file
+pthread_mutex_t mutex;		// mutex for theadTime
 
-double threadTime[NUM_THREADS] = {1,1,1,1,1,1,1,1};
+double threadTime[NUM_THREADS] = {1, 1, 1, 1, 1, 1, 1, 1};
 
 int threadInfo[NUM_THREADS][3] = {};
 
+int maxCharArray[1000000] = {};
 
-//This function given a line of characters returns the max ascii value from all the characters in the line
-void *getLineAndMax(void *threadIndex){
-	
-	/* Trys to open file */
-	char* src_file = "/homes/dan/625/wiki_dump.txt";
-	//char* src_file = "/homes/brenth01/hw4/3way-pthread/test.txt"; // Test
-	FILE *file  = fopen(src_file, "r");
-	if (file == NULL) {
-		printf("File did not open");
-		exit(-1);
-	}
-	clock_t start, end;
-	
-	int threadNum = (int)threadIndex;
-    int startLine = threadInfo[threadNum][0];
-    int endLine = threadInfo[threadNum][1];
-	int lineNum = 0;
-	char *line = NULL;
+char *src_file = "/homes/dan/625/wiki_dump.txt";
+//char *src_file = "names.txt";
+
+void *getLineAndMax(void *myID)
+{
+    int threadNumber = (int*)myID;
+    printf("Thread: %d", threadNumber);
+
+    FILE *file = fopen(src_file, "r");
+    if (file == NULL)
+    {
+        printf("File did not open");
+        exit(-1);
+    }
+    int startLine = threadInfo[threadNumber][0];
+    int endLine = threadInfo[threadNumber][1];
+
+    fseek(file, startLine, SEEK_SET);
+
+    int lineNum = startLine;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
-	start = clock();
-	fseek(file, startLine, SEEK_SET);
+    clock_t start = clock();
 
-	while ((read = getline(&line, &len, file)) != -1 && lineNum < endLine) {
-		lineNum++;
-		int i = 0;
-		char currentCharacter = line[i];
-		char maxCharacter = line[i];
-		while (currentCharacter != '\n') {
-			if ((int)currentCharacter > (int)maxCharacter) {
-				maxCharacter = currentCharacter;
-			}
-			i++;
-			currentCharacter = line[i];
-		}
-	}
-	free(line);
-	end = clock();
-	threadTime[threadNum] = ((double)(end - start)) / CLOCKS_PER_SEC;
-	printf("Thead Number %d ", threadNum);
-	printf("Thread Time %f \n", threadTime[threadNum]);
-	fclose(file);
+    while ((read = getline(&line, &len, file)) != -1 && lineNum < endLine)
+    {
+        int i = 0;
+        char currentCharacter = line[i];
+        char maxCharacter = line[i];
+        while (currentCharacter != '\n')
+        {
+            if ((int)currentCharacter > (int)maxCharacter)
+            {
+                maxCharacter = currentCharacter;
+            }
+            i++;
+            currentCharacter = line[i];
+        }
+        pthread_mutex_lock(&mutex);
+        maxCharArray[lineNum] = maxCharacter;
+        pthread_mutex_unlock(&mutex);
+        lineNum++;
+    }
+    free(line);
+    fclose(file);
+
+    clock_t end = clock();
+    double elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    pthread_mutex_lock(&mutex);
+    threadTime[threadNumber] = elapsedTime;
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
 }
 
 int main() {
@@ -64,77 +77,83 @@ int main() {
 	pthread_t threads[NUM_THREADS];
 	pthread_attr_t attr;
 	void *status;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
 	/* Initialize and set thread detached attribute */
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	
-	/* Trys to open file */
-	char* src_file = "/homes/dan/625/wiki_dump.txt";
-	//char* src_file = "/homes/brenth01/hw4/3way-pthread/test.txt"; // Test
-	FILE *file  = fopen(src_file, "r");
-	if (file == NULL) {
-		printf("File did not open");
-		exit(-1);
-	}
-	
-	/* Calculate the total number of lines in the file */
+
+	pthread_mutex_init(&mutex, NULL);
+
+    FILE *file = fopen(src_file, "r");
+    if (file == NULL)
+    {
+        printf("File did not open");
+        exit(-1);
+    }
+
     int totalLines = 0;
-	fseek(file, 0, SEEK_SET);
-    while ((read = getline(&line, &len, file)) != -1) {
+    fseek(file, 0, SEEK_SET);
+    while ((read = getline(&line, &len, file)) != -1)
+    {
         totalLines++;
     }
-	fclose(file);
-	/* Calculates amount of lines that a single tread runs */
-	int linesPerThread = totalLines / NUM_THREADS;
+    fclose(file);
+
+    int linesPerThread = totalLines / NUM_THREADS;
     int remainder = totalLines % NUM_THREADS;
     int startLine = 1;
-	
-	
 
-	/* Makes theads and runs them with the given amount of lines */
 	for (i = 0; i < NUM_THREADS; i++ ) {
-		int endLine = startLine + linesPerThread - 1;
-		if (i < remainder) {
+        int endLine = startLine + linesPerThread - 1;
+        if (i < remainder)
+        {
             endLine++;
         }
-		//pthread_mutex_lock(&mutex);
-		threadInfo[i][0] = startLine;
-		threadInfo[i][1] = endLine;
-		threadInfo[i][2] = i;
+
+        if (endLine > totalLines) {
+            endLine = totalLines;
+}       else {
+            endLine = endLine;
+        }
+    
+        threadInfo[i][0] = startLine;
+        threadInfo[i][1] = endLine;
+        threadInfo[i][2] = i;
         startLine = endLine + 1;
-		
-		rc = pthread_create(&threads[i], &attr, getLineAndMax, (void *)i); 
-		//pthread_mutex_unlock(&mutex);
-		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
+
+	      rc = pthread_create(&threads[i], &attr, getLineAndMax, (void *)i);
+	      if (rc) {
+	        printf("ERROR; return code from pthread_create() is %d\n", rc);
+		exit(-1);
+	      }
 	}
 
 	/* Free attribute and wait for the other threads */
 	pthread_attr_destroy(&attr);
-	for (i=0; i<NUM_THREADS; i++) {
-		rc = pthread_join(threads[i], &status);
-		if (rc) {
-		printf("ERROR; return code from pthread_join() is %d\n", rc);
-		exit(-1);
-		}
+	for(i=0; i<NUM_THREADS; i++) {
+	     rc = pthread_join(threads[i], &status);
+	     if (rc) {
+		   printf("ERROR; return code from pthread_join() is %d\n", rc);
+		   exit(-1);
+	     }
 	}
 
-	/* Print results */
 	printf("Average Lines Per Thread: %d\n", linesPerThread);
-	for (i = 0; i < NUM_THREADS; i++) {
-		printf("Thread: %d, Time: %lf\n", i, threadTime[i]);
-	}
-	
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        printf("Thread: %d, Time: %lf\n", i, threadTime[i]);
+    }
+
+    /* This is for the assignment
+    for (i = 0; i < sizeof(maxCharArray);i++){
+        printf("%d", maxCharArray[i]);
+    }
+    */
 	pthread_mutex_destroy(&mutex);
 	printf("Main: program completed. Exiting.\n");
 	pthread_exit(NULL);
-
-	
 }
 
