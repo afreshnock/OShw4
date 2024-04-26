ac83b094ecaf5583b9188ea0b9a458df29be26af
Brent H <73561551+Maxfly2001@users.noreply.github.com>
2024-04-26 13:13:07 -0500

    This is the actual working pt3

--- pt3.c
+++ pt3.c
@@ -2,106 +2,48 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define NUM_THREADS 4
-#define MAXFILESIZE 1000000
-
-double threadTime[NUM_THREADS] = {};
-
-int threadInfo[NUM_THREADS][3] = {};
-
-int *maxCharArray = NULL;
-
-char **fileArray = NULL;
-
-void getLineAndMax(int threadNumber, char *src_file, int startLine, int endLine) 
-{
-    clock_t start = clock();
-    int maxCharArrayLocal[MAXFILESIZE];
-    for (int i = startLine; i < endLine; i++) 
-    {
-        char *line = fileArray[i];
-        size_t len = 0;
-        int j = 0;
-        char currentCharacter = line[j];
-        char maxCharacter = line[j];
-        while (currentCharacter != '\n')
-        {
-            if ((int)currentCharacter > (int)maxCharacter)
-            {
-                maxCharacter = currentCharacter;
-            }
-            j++;
-            currentCharacter = line[j];
-        }
-        maxCharArray[i] = maxCharacter;
-    }
-    clock_t end = clock();
-    double elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
-    threadTime[threadNumber] = elapsedTime;
-}
+#define MAX_NAME_LENGTH 1000
+#define NUM_LINES 1000000
 
 int main() {
-    clock_t start = clock();
-    int extraLines = 0;
-    size_t len = 0;
-    char *line = NULL;
-    //char *src_file = "/homes/dan/625/wiki_dump.txt";
-    char *src_file = "names.txt";
-    FILE *file = fopen(src_file, "r");
-    if (file == NULL)
-    {
-        printf("File did not open");
-        exit(-1);
-    }
-
-    fileArray = (char **)malloc(MAXFILESIZE * sizeof(char *));
+    FILE *file;
+    char name[MAX_NAME_LENGTH];
+    char maxCharArray[NUM_LINES];
     
-    if (fileArray == NULL) {
-        printf("Memory allocation failed\n");
-        exit(-1);
+    file = fopen("names.txt", "r");
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
     }
+    clock_t start = omp_get_wtime();
     
-    int totalLines = 0;
-    fseek(file, 0, SEEK_SET);
-    while (getline(&line, &len, file) != -1)
+    #pragma omp parallel
     {
-        fileArray[totalLines] = strdup(line);
-        totalLines++;
+        char maxCharacter;
+        #pragma omp for schedule(dynamic, 1)
+        for (int i = 0; i < NUM_LINES; i++) {
+            fgets(name, MAX_NAME_LENGTH, file);
+            maxCharacter = name[0];
+            for (int j = 1; name[j] != '\0'; j++) {
+                if (name[j] > maxCharacter) {
+                    maxCharacter = name[j];
+                }
+            }
+            maxCharArray[i] = maxCharacter;
+        }
+        double end = omp_get_wtime(); 
+        printf("Thread: %d time: %lf \n", omp_get_thread_num(), end - start);
     }
     fclose(file);
-
-    int linesPerThread = totalLines / NUM_THREADS;
-    int remainder = totalLines % NUM_THREADS;
-    int startLine = 0;
-    int endLine = 0;
-
-    maxCharArray = (int *)malloc(totalLines * sizeof(int));
-
-    #pragma omp parallel num_threads(NUM_THREADS)
-    {
-        int tid = omp_get_thread_num();
-        startLine = tid * linesPerThread;
-        endLine = (tid + 1) * linesPerThread;
-        getLineAndMax(tid, src_file, startLine, endLine);
-    }
-
     clock_t end = clock();
     double elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("TOtal Time %lf\n", elapsedTime);
-    printf("Average Lines Per Thread: %d\n", linesPerThread);
-    for (int i = 0; i < NUM_THREADS; i++)
-    {
-        printf("Thread: %d, Time: %lf\n", i, threadTime[i]);
+    printf("Total Time %lf\n", elapsedTime);
+
+    for (int i = 0; i < NUM_LINES; i++) {
+        printf("%d ", (int)maxCharArray[i]);
     }
-    
-    //This is for the assignment
-    for (int i = 0; i < totalLines;i++){
-        printf("%d\n", maxCharArray[i]);
-    }
+    printf("\n");
     
     return 0;
 }
